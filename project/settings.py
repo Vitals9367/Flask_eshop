4ccf2840e815fadedbf7db3657c344cf03aed75e
Vitals9367 <Vitals9367@users.noreply.github.com>
2020-11-23 20:47:52 +0200

    obd works

--- project/settings.py
+++ project/settings.py
@@ -5,12 +5,11 @@ import os
 import urllib.parse
 
 params = urllib.parse.quote_plus(
-    "DRIVER={SQL Server};SERVER=xamdb.database.windows.net;DATABASE=XamarinDB;UID=vitalijusal9367;PWD=mariukas1A.")
+    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=xamdb.database.windows.net;DATABASE=XamarinDB;UID=vitalijusal9367;PWD=mariukas1A.")
 
 
 class Config:
     SECRET_KEY = '6f137bcbe4bff73bc8f6f4c59a3c5029'
-    FLASK_ENV = 'development'
 
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
     SQLALCHEMY_ECHO = True
